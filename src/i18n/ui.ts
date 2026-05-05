5a47b00a227d978d42884dfd24685a80ca009179
mibodaperfecta <arubio.xheras@gmail.com>
2026-05-05 20:19:25 +0200

    added light-dark switch, with default auto. Add base translation for future use

---
+++ src/i18n/ui.ts
@@ -0,0 +1,19 @@
+export const defaultLang = "es";
+
+export const ui = {
+  es: {
+    "theme.light": "Claro",
+    "theme.dark": "Oscuro",
+    "theme.auto": "Auto",
+  },
+  ca: {
+    "theme.light": "Clar",
+    "theme.dark": "Fosc",
+    "theme.auto": "Auto",
+  },
+  en: {
+    "theme.light": "Light",
+    "theme.dark": "Dark",
+    "theme.auto": "Auto",
+  },
+} as const;

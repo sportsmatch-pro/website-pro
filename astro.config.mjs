2d6b12c7cd141d85eb28308b234c52d90d463655
Oriol Ferràndez Grau <oriol@ferrandez.cat>
2026-06-01 10:14:47 +0200

    use custom port

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,12 @@
 import react from '@astrojs/react'
 import tailwindcss from '@tailwindcss/vite'
+import { loadEnv } from 'vite'
+
+const env = loadEnv('', process.cwd(), '')
+const PORT = parseInt(env.PORT || '3002', 10)
 
 export default {
+    server: { port: PORT },
     integrations: [
         react()
     ],
